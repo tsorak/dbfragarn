c2c6765ccfa8cdf160ed3e591c05658dda2ce094
tsorak <karost0249@gmail.com>
2023-02-07 14:29:32 +0100

    Run a simple query

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ async fn main() -> Result<(), sqlx::Error> {
     let db_url = match env::var("URL") {
         Ok(v) => String::from(v),
         Err(_) => {
-            println!("Please assign a URL environment variable. Template URL: 'mysql://USERNAME:PASSWORD@HOST/DATABASE'");
+            println!("Please assign a URL environment variable.");
+            println!("Template URL: 'mysql://USERNAME:PASSWORD@HOST/DATABASE'");
             process::exit(1);
         }
     };
@@ -16,13 +17,17 @@ async fn main() -> Result<(), sqlx::Error> {
         .connect(&db_url)
         .await?;
 
-    // Make a simple query to return the given parameter
-    let row: (i64,) = sqlx::query_as("SELECT ?")
-        .bind(150_i64)
+    // Make a simple query
+    let query = "SELECT 1+1";
+
+    println!("{}", query);
+
+    let row: (i32,) = sqlx::query_as(query)
+        // .bind(150_i64)
         .fetch_one(&pool)
         .await?;
 
-    assert_eq!(row.0, 150);
+    println!("{:#?}", row);
 
     Ok(())
 }
