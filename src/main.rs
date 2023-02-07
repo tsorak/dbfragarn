ecca6fb2901998ddadb732cce8023c672bf9aeb4
tsorak <karost0249@gmail.com>
2023-02-07 11:58:28 +0100

    Add crate sqlx MySQL template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::mysql::MySqlPoolOptions;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    // Create a connection pool
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect("mysql://mysql:password@localhost/test")
+        .await?;
+
+    // Make a simple query to return the given parameter
+    let row: (i64,) = sqlx::query_as("SELECT ?")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await?;
+
+    assert_eq!(row.0, 150);
+
+    Ok(())
 }
