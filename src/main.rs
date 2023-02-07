8e435189691078344628314fa3f2db1dc780c39d
tsorak <karost0249@gmail.com>
2023-02-07 13:05:25 +0100

    Get database URL from env

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use sqlx::mysql::MySqlPoolOptions;
+use std::{env, process};
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
+    let db_url = match env::var("URL") {
+        Ok(v) => String::from(v),
+        Err(_) => {
+            println!("Please assign a URL environment variable. Template URL: 'mysql://USERNAME:PASSWORD@HOST/DATABASE'");
+            process::exit(1);
+        }
+    };
     // Create a connection pool
     let pool = MySqlPoolOptions::new()
         .max_connections(5)
-        .connect("mysql://mysql:password@localhost/test")
+        .connect(&db_url)
         .await?;
 
     // Make a simple query to return the given parameter
